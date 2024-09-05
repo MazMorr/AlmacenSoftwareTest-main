57778e4fc57d6dae0ec482b44fc9dd4d000c5caa
Marco-Romero <122071151+MazMorr@users.noreply.github.com>
2024-09-05 18:59:25 -0400

    Add files via upload

---
+++ src/excepciones/Exception_Product_Not_Found.java
@@ -0,0 +1,10 @@
+package excepciones;
+
+import javax.swing.JOptionPane;
+
+public class Exception_Product_Not_Found extends Exception{
+
+    public  Exception_Product_Not_Found(){
+        super("Product Not Found");
+    }
+}
